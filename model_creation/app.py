f096339130ea8931a27ac4b6913030fc3a9b1fe2
Sharanya Saha <113009994+sharanyax001@users.noreply.github.com>
2023-06-15 15:59:36 +0530

    Add files via upload

---
+++ model_creation/app.py
@@ -0,0 +1,17 @@
+import pickle
+import string
+import streamlit as st
+import webbrowser
+
+global Lrdetect_Model
+
+LrdetectFile = open('model.pckl', 'rb')
+Lrdetect_Model = pickle.load(LrdetectFile)
+LrdetectFile.close()
+st.title("Language detection tool")
+input_test = st.text_input(
+    "provide your text input here", 'Hello my name is Sharanya ')
+
+button_clicked = st.button("Get Language Name")
+if button_clicked:
+    st.text(Lrdetect_Model.predict([input_test]))
